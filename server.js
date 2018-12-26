63573690a80b5f10605d0100eb1de2608b9b4c2d
jquirossoto <jquirossoto@gmail.com>
2018-12-26 15:45:53 -0600

    portfolio view

--- server.js
+++ server.js
@@ -42,6 +42,13 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Portfolio',
+        welcomeMessage: 'My projects here!'
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
